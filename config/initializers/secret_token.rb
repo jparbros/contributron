ac4720cd86b335eaf5da00fb83721eb023f8e7e8
Hector Bustillos <hector.bustillos@crowdint.com>
2014-04-10 21:40:27 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Contributron::Application.config.secret_key_base = '84cae6e76f7f487cbedeee522f56bbb3a3f9a1a83a979188c8cf5a38a6c50fca0f54965dfa1492ec61ebece3a607ce11ee14ea68940ea05a137f71efeb39acb2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Contributron::Application.config.session_store :cookie_store, key: '_contributron_session'
