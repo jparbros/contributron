75eedf431d63fc5567ec487c8a060dd42de0a27e
Hector Bustillos <hector.bustillos@crowdint.com>
2014-04-16 10:23:42 -0500

    call correct method in PRs worker

--- app/workers/pull_requests_worker.rb
+++ app/workers/pull_requests_worker.rb
@@ -4,6 +4,6 @@ class PullRequestsWorker
 
   def perform(repository_id, token)
     repository = Repository.find repository_id
-    repository.start_bulk(token)
+    repository.process_pull_requests(token)
   end
 end
